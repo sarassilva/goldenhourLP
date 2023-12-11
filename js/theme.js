ccba975b567bf9f0f084af7778efbc2fcdc1362c
Sara <31486459+sarassilva@users.noreply.github.com>
2023-12-11 15:59:34 -0300

    Upload LP

---
+++ js/theme.js
@@ -0,0 +1,8 @@
+//video pop up
+const popup = document.querySelector('.popup');
+const overlay = document.querySelector('.overlay');
+
+function play() {
+    popup.classList.toggle('show');
+    overlay.classList.toggle('show');
+}
